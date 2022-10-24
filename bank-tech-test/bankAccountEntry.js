d93532899cc92753d6f38c305bb412ae1c281c05
D. Dramchev <ddramchev@gmail.com>
2022-10-24 20:04:48 +0100

    Add BankAccountEntry class

---
+++ bank-tech-test/bankAccountEntry.js
@@ -0,0 +1,45 @@
+const Transaction = require("./transaction");
+
+class BankAccountEntry {
+  constructor(initialBalance, type, amount) {
+    if (type != "credit" && type != "debit") {
+      throw "Error: Type should be 'debit' or 'credit'";
+    }
+    if (!Number.isInteger(amount)) {
+      throw "Error: Amount should be an integer";
+    }
+    if (amount <= 0) {
+      throw "Error: Amount should be positive";
+    }
+    this.transaction = new Transaction(type, amount);
+    if (type === "credit") {
+      this.resultingBalance = initialBalance + amount;
+    }
+    if (type === "debit") {
+      this.resultingBalance = initialBalance - amount;
+    }
+  }
+
+  getType() {
+    if (this.transaction.isDebit()) {
+      return "debit";
+    }
+    if (this.transaction.isCredit()) {
+      console.log(
+        "THE ABOVE LINE ACTUALLY RUNS IN THE TEST",
+        "BUT APPEARS AS YELLOW IN COVERAGE REPORT"
+      );
+      return "credit";
+    }
+  }
+
+  getAmount() {
+    return this.transaction.getAmount();
+  }
+
+  getResultingBalance() {
+    return this.resultingBalance;
+  }
+}
+
+module.exports = BankAccountEntry;
---
+++ bank-tech-test/bankAccountEntry.test.js
@@ -0,0 +1,78 @@
+const Transaction = require("./transaction");
+const BankAccountEntry = require("./bankAccountEntry");
+
+jest.mock("./transaction");
+
+afterEach(() => {
+  Transaction.mockClear();
+});
+
+describe("BankAccountEntry", () => {
+  it("raises error if amount is a string", () => {
+    expect(() => {
+      new BankAccountEntry(0, "debit", "5");
+    }).toThrow("Error: Amount should be an integer");
+  });
+
+  it("raises error if amount is a float", () => {
+    expect(() => {
+      new BankAccountEntry(0, "debit", 5.5);
+    }).toThrow("Error: Amount should be an integer");
+  });
+
+  it("raises error if amount is negative", () => {
+    expect(() => {
+      new BankAccountEntry(0, "debit", -10);
+    }).toThrow("Error: Amount should be positive");
+  });
+
+  it("raises error if amount is zero", () => {
+    expect(() => {
+      new BankAccountEntry(0, "debit", 0);
+    }).toThrow("Error: Amount should be positive");
+  });
+
+  it("raises error if type is not 'credit' or 'debit'", () => {
+    expect(() => {
+      new BankAccountEntry(0, "invalid value", 10);
+    }).toThrow("Error: Type should be 'debit' or 'credit'");
+  });
+
+  it("creates a credit account entry", () => {
+    const initialBalance = 1000;
+    const type = "credit";
+    const amount = 200;
+
+    Transaction.mockImplementation(() => {
+      return {
+        isDebit: () => false,
+        isCredit: () => true,
+        getAmount: () => 200,
+      };
+    });
+
+    entry = new BankAccountEntry(initialBalance, type, amount);
+    expect(entry.getType()).toEqual("credit");
+    expect(entry.getAmount()).toEqual(200);
+    expect(entry.getResultingBalance()).toEqual(1200);
+  });
+
+  it("creates a debit account entry", () => {
+    const initialBalance = 1000;
+    const type = "debit";
+    const amount = 200;
+
+    Transaction.mockImplementation(() => {
+      return {
+        isDebit: () => true,
+        isCredit: () => false,
+        getAmount: () => 200,
+      };
+    });
+
+    entry = new BankAccountEntry(initialBalance, type, amount);
+    expect(entry.getType()).toEqual("debit");
+    expect(entry.getAmount()).toEqual(200);
+    expect(entry.getResultingBalance()).toEqual(800);
+  });
+});
